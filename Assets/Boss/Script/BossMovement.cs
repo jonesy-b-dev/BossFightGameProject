e0df996b38f027caa0987060d53b5a08d1a5e8c6
Jonas de Bruin <59418915+jonesy-b-dev@users.noreply.github.com>
2022-11-29 15:31:13 +0100

    Added trigger

--- Assets/Boss/Script/BossMovement.cs
+++ Assets/Boss/Script/BossMovement.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BossMovement : MonoBehaviour
 {
+    [SerializeField] private int bossMovementSpeed = 5;
+    [SerializeField]
+    private bool chaseActivated = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,14 @@ public class BossMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (chaseActivated)
+        {
+          transform.Translate(Vector2.right * bossMovementSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("ben");
     }
 }
