bce02299b9f6f73be8b7c2f9191def1e1d9aa237
Jonas de Bruin <59418915+jonesy-b-dev@users.noreply.github.com>
2022-11-30 12:57:17 +0100

    Random attacks (sort of...)

--- Assets/Boss/Script/BossMovement.cs
+++ Assets/Boss/Script/BossMovement.cs
@@ -1,14 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 
 public class BossMovement : MonoBehaviour
 {
     [SerializeField] private int bossMovementSpeed = 5;
-    [SerializeField]
-    private bool chaseActivated = true;
+    [SerializeField] private bool chaseActivated = true;
+    [SerializeField] private int bossHP = 1000;
 
+    private bool inAttckstage = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,12 +22,65 @@ public class BossMovement : MonoBehaviour
     {
         if (chaseActivated)
         {
-          transform.Translate(Vector2.right * bossMovementSpeed * Time.deltaTime);
+            //Chasing state
+            transform.Translate(Vector2.right * bossMovementSpeed * Time.deltaTime);
+        }
+        else
+        {
+            //Attack state
+            if (!inAttckstage)
+            {
+                inAttckstage = true;
+                StartCoroutine(StartAttackPhase());
+            }
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    IEnumerator StartAttackPhase()
     {
-        Debug.Log("ben");
+        System.Random rand = new System.Random();
+
+        while (bossHP >= 0)
+        {
+            int nextAttack = rand.Next(1, 5);
+            switch (nextAttack)
+            {
+                case 1:
+                    SweepAttack();
+                    break;
+                case 2:
+                    BodySlam();
+                    break;
+                case 3:
+                    ProjectileAttack();
+                    break;
+                case 4:
+                    ProjectileRainAttack();
+                    break;
+                default:
+                    break;
+            }
+            yield return new WaitForSeconds(2);
+        }
     }
-}
+
+    private void ProjectileRainAttack()
+    {
+        Debug.Log("1");
+    }
+
+    private void ProjectileAttack()
+    {
+        Debug.Log("2");
+    }
+
+    private void BodySlam()
+    {
+        Debug.Log("3");
+    }
+
+    private void SweepAttack()
+    {
+        Debug.Log("4");
+    }
+}   
