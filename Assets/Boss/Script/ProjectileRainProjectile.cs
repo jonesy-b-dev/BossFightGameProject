79acd3dda8ec21657fb206514112c57a7e9b9c68
Jonas de Bruin <59418915+jonesy-b-dev@users.noreply.github.com>
2022-12-07 10:23:53 +0100

    Anti Aliasing + better random for bullet rain

--- Assets/Boss/Script/ProjectileRainProjectile.cs
+++ Assets/Boss/Script/ProjectileRainProjectile.cs
@@ -7,7 +7,7 @@ public class ProjectileRainProjectile : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector2(Random.Range(-20, 20), Random.Range(6, 26));
+        transform.position = new Vector2(Random.Range(-20f, 20f), Random.Range(6f, 26f));
 
         Destroy(gameObject, 3);
     }
